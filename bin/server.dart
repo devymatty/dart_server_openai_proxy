0b727162f4ecfedffe6ca34534287e994a955ed8
matty <devymatty@gmail.com>
2024-03-14 11:24:46 +0300

    wip

--- bin/server.dart
+++ bin/server.dart
@@ -1,42 +1,28 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-Future<void> main() async {
-  // Создание HTTP сервера, который слушает localhost на порту 8080
-  var server = await HttpServer.bind('localhost', 8080);
-  print('Сервер запущен на http://${server.address.host}:${server.port}');
+void main() async {
+  var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 8080);
+  print('Прокси-сервер запущен на http://${server.address.address}:${server.port}');
 
-  // Обработка всех входящих запросов
-  await for (var request in server) {
-    // URL-адрес, на который будут перенаправляться запросы
-    var targetUrl = Uri.parse('https://api.openai.com');
-
-    // Создание нового URI с заменой схемы, хоста и порта на целевые,
-    // но сохранением пути и параметров запроса
-    var forwardedUri = targetUrl.replace(
-      path: request.uri.path,
-      queryParameters: request.uri.queryParameters,
-    );
-
-    print(forwardedUri);
-    print(targetUrl);
-
+  await for (HttpRequest request in server) {
+    var client = http.Client();
     try {
-      // Перенаправление запроса на целевой URL
-      var response = await http.get(forwardedUri);
+      // Измените URL на адрес API, к которому вы хотите обратиться
+      var uri = Uri.parse('https://api.openai.com' + request.uri.path);
+      var response = await client.get(uri);
 
-      // Пересылка ответа от целевого сервера клиенту
       request.response
         ..statusCode = response.statusCode
-        ..headers.contentType = ContentType.parse(response.headers['content-type']!)
         ..write(response.body)
         ..close();
     } catch (e) {
-      // Обработка ошибок, например, если целевой сервер недоступен
       request.response
         ..statusCode = HttpStatus.internalServerError
-        ..write('Произошла ошибка: $e')
+        ..write('Ошибка: $e')
         ..close();
+    } finally {
+      client.close();
     }
   }
 }
